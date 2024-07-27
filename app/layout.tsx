138d6656bcbd82b6bc4e77387ac22e6f19295f65
Justice Opara <justiceopara33@gmail.com>
2024-07-27 16:46:57 +0100

    added themcontext

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/context/ThemeProvider";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider>
+        {children}
+        <Analytics/>
+        </ThemeProvider>
+    
+        </body>
     </html>
   );
 }
---
+++ context/ThemeProvider.tsx
@@ -0,0 +1,55 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import dark from "@clerk/themes";
+import React, { createContext, useContext, useState, useEffect } from "react";
+import { Toaster } from "sonner";
+
+interface ThemContextType {
+  mode: string;
+  setMode: (mode: string) => void;
+}
+
+const ThemeContext = createContext<ThemContextType | undefined>(undefined);
+export function ThemeProvider({ children } : { children : React.ReactNode }) {
+  const [mode, setMode] = useState("dark");
+
+  const handleThemeChange = () => {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      setMode("dark");
+      document.documentElement.classList.add("dark");
+    } else {
+      setMode("light");
+      document.documentElement.classList.remove("dark");
+    }
+  };
+  useEffect(() => {
+    handleThemeChange();
+  }, [mode]);
+
+  return (
+
+      <ThemeContext.Provider value={{ mode, setMode }}>
+        <ClerkProvider  appearance={{
+          baseTheme: mode === "dark" ? dark : undefined,
+        }}>
+            {children}
+        </ClerkProvider>
+        <Toaster richColors theme={mode === "dark" ? "dark" : "light"} />
+      </ThemeContext.Provider>
+    
+  );
+}
+export function useTheme() {
+    const context = useContext(ThemeContext);
+  
+    if (context === undefined) {
+      throw new Error("useTheme must be used within a ThemeProvider");
+    }
+  
+    return context;
+  }
+
+
